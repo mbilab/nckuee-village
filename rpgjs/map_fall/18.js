575f336fa9d5f33614df5fb4c600ffbd8dd882f3
dirty <darby.chang@gmail.com>
2014-01-16 08:29:06 +0800

    ids

---
+++ rpgjs/map_fall/18.js
@@ -0,0 +1,44 @@
+var s = game.Ev.prototype.cmd.script, t = game.Ev.prototype.cmd.text, v0 = game.Ev.prototype.cmd.v0;
+var map = 1, id = 18, ev = 'game.ev['+map+']['+id+']', name = '計算機組織';
+game.ev[map][id] = new game.Ev({
+	can_take: function() {
+		if ( !game.defined( game, 'ev', 1, 4, 'took' ) ) RPGJS.Variables.data[0] = '需要先修 '+game.ev[1][4].name+' ！';
+		else if ( G.player.hp < this.hp_cost() ) RPGJS.Variables.data[0] = '你的體力不夠修這門課囉！';
+		else RPGJS.Variables.data[0] = 1;
+	},
+	hp_cost: function() { return 10; },
+	id: id,
+	map: map,
+	name: name,
+}, [
+	s(ev+'.stop()'),
+	t('計算機概論教你軟體方面的知識，計算機組織則偏重硬體方面，你將可以學到機器語言以及硬體的執行方式、時程與順序，修完課後，你將會更加瞭解CPU以及CPU在電腦中扮演的角色。'),
+	t('你要修 '+name+' 嗎？'),
+	'CHOICES: ["是","否"]',
+	'CHOICE_0',
+		s(ev+'.is_took()'),
+		'IF: "0 == variable[0]"',
+			s(ev+'.can_take()'),
+			'IF: "1 == variable[0]"',
+				t('請問MIPS指令中，add $s1,$s2,$s3 的意思是？'),
+				'CHOICES: ["將$s1的資料與$s2的資料相加，存到$s3","將$s2的資料與$s3的資料相加，存到$s1","將$s1的資料與$s3的資料相加，存到$s2","將$s1的資料與$s2還有$s3的資料相加，存到$s3"]',
+				'CHOICE_0',
+					s(ev+'.fail("答錯了！ＭＩＰＳ指令的順序與一邊我們熟知的運算順序是不同的喔～正確的答案是將$s2的資料與$s3的資料相加，存到$s1")'),
+				'CHOICE_1',
+					s(ev+'.take("恭喜答對了！這是給機器看的語言喔～現在你也懂了，真是太棒了！")'),
+				'CHOICE_2',
+					s(ev+'.fail("答錯了！ＭＩＰＳ指令的順序與一邊我們熟知的運算順序是不同的喔～正確的答案是將$s2的資料與$s3的資料相加，存到$s1")'),
+				'CHOICE_3',
+					s(ev+'.fail("答錯了！ＭＩＰＳ指令的順序與一邊我們熟知的運算順序是不同的喔～正確的答案是將$s2的資料與$s3的資料相加，存到$s1")'),
+				'ENDCHOICES',
+			'ENDIF',
+		"ENDIF",
+	'CHOICE_1',
+	'ENDCHOICES',
+	'IF: "0 != variable[0]"',
+		t('%V[0]'),
+	"ENDIF",
+	s(ev+'.start()'),
+]);
+
+// vi:nowrap:sw=4:ts=4
---
+++ rpgjs/map_spring/44.js
@@ -0,0 +1,44 @@
+var s = game.Ev.prototype.cmd.script, t = game.Ev.prototype.cmd.text, v0 = game.Ev.prototype.cmd.v0;
+var map = 2, id = 44, ev = 'game.ev['+map+']['+id+']', name = '資料結構緒論';
+game.ev[map][id] = new game.Ev({
+	can_take: function() {
+		if ( !game.defined( game, 'ev', 2, 4, 'took' ) ) RPGJS.Variables.data[0] = '需要先修 '+game.ev[1][4].name+' ！';
+		else if ( G.player.hp < this.hp_cost() ) RPGJS.Variables.data[0] = '你的體力不夠修這門課囉！';
+		else RPGJS.Variables.data[0] = 1;
+	},
+	hp_cost: function() { return 8; },
+	id: id,
+	map: map,
+	name: name,
+}, [
+	s(ev+'.stop()'),
+	t('資料結構主要是在研究如何把資料，組織、安排、存放到電腦中的一門學問。如果學好資料結構對於節省儲存空間、增加資料的安全性與處理速度將有很大幫助，對日後在軟、硬體發展上有彌足深遠的影響。'),
+	t('你要修 '+name+' 嗎？'),
+	'CHOICES: ["是","否"]',
+	'CHOICE_0',
+		s(ev+'.is_took()'),
+		'IF: "0 == variable[0]"',
+			s(ev+'.can_take()'),
+			'IF: "1 == variable[0]"',
+				t('請問下列何者是儲存資料的形式？'),
+				'CHOICES: ["佇列（Ｑｕｅｕｅ）","堆疊（Ｓｔａｃｋ）","樹形（Ｔｒｅｅ）","以上皆是"]',
+				'CHOICE_0',
+					s(ev+'.fail("答錯了＞＜！！堆疊跟樹形也都是儲存資料的一種形式喔～")'),
+				'CHOICE_1',
+					s(ev+'.fail("答錯了＞＜！！序列跟樹形也都是儲存資料的一種形式喔～")'),
+				'CHOICE_2',
+					s(ev+'.fail("答錯了＞＜！！序列跟堆疊也都是儲存資料的一種形式喔～")'),
+				'CHOICE_3',
+					s(ev+'.take("恭喜答對了！相信有的基本概念，對於以後的學習一定有幫助，加油！")'),
+				'ENDCHOICES',
+			'ENDIF',
+		"ENDIF",
+	'CHOICE_1',
+	'ENDCHOICES',
+	'IF: "0 != variable[0]"',
+		t('%V[0]'),
+	"ENDIF",
+	s(ev+'.start()'),
+]);
+
+// vi:nowrap:sw=4:ts=4
