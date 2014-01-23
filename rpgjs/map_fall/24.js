b4e524fc3c6fbd08368aa9924a9a46277490d702
dirty <darby.chang@gmail.com>
2014-01-23 22:16:24 +0800

    Finish system programming.

--- rpgjs/map_fall/24.js
+++ rpgjs/map_fall/24.js
@@ -1,7 +1,7 @@
 var s = game.Ev.prototype.cmd.script, t = game.Ev.prototype.cmd.text, v0 = game.Ev.prototype.cmd.v0;
 var map = 1, id = 24, ev = 'game.ev['+map+']['+id+']', name = '系統程式';
 game.ev[map][id] = new game.Ev({
-	hp_cost: function() { return 18; },
+	hp_cost: function() { return 10; },
 	id: id,
 	map: map,
 	name: name,
@@ -18,13 +18,13 @@ game.ev[map][id] = new game.Ev({
 				t('身為一個電腦工程師，必定會使用過編譯器(Compiler)，那編譯器是怎麼把工程師所寫的code轉變成電腦看得懂得語言呢？'),
 				'CHOICES: ["Source code -> Lexical analysis -> Semantic analysis -> Syntactic analysis -> Code generation","Source code -> Lexical analysis -> Syntactic analysis -> Semantic analysis -> Code generation","Source code -> Semantic analysis -> Lexical analysis -> Syntactic analysis -> Code generation","Source code -> Syntactic analysis -> Lexical analysis -> Semantic analysis -> Code generation"]',
 				'CHOICE_0',
-					s(ev+'.fail("n/a")'),
+					s(ev+'.fail("好可惜，答錯了喔！！請再仔細想想！！")'),
 				'CHOICE_1',
-					s(ev+'.fail("n/a")'),
+					s(ev+'.take("恭喜你答對了！首先compiler會把工程師寫的code讀進來分析，然後就會像數學加減乘除一樣把code作適當的排列再產生可以執行的檔案喔！！")'),
 				'CHOICE_2',
-					s(ev+'.fail("n/a")'),
+					s(ev+'.fail("好可惜，答錯了喔！！請再仔細想想！！")'),
 				'CHOICE_3',
-					s(ev+'.fail("n/a")'),
+					s(ev+'.fail("好可惜，答錯了喔！！請再仔細想想！！")'),
 				'ENDCHOICES',
 			'ENDIF',
 		"ENDIF",
