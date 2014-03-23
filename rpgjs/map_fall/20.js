440441a6144f2d2017e8ebcf0b7cb83da48b1fae
q5183t251 <s0a6m45@yahoo.com.tw>
2014-03-23 23:30:40 +0800

    modified 20.js & 29.js

--- rpgjs/map_fall/20.js
+++ rpgjs/map_fall/20.js
@@ -1,13 +1,13 @@
 var s = game.Ev.prototype.cmd.script, t = game.Ev.prototype.cmd.text, v0 = game.Ev.prototype.cmd.v0;
-var map = 1, id = 29, ev = 'game.ev['+map+']['+id+']', name = '電力設備';
+var map = 1, id = 20, ev = 'game.ev['+map+']['+id+']', name = '機率與統計';
 game.ev[map][id] = new game.Ev({
-	hp_cost: function() { return 5; },
+	hp_cost: function() { return 15; },
 	id: id,
 	map: map,
 	name: name,
 }, [
 	s(ev+'.stop()'),
-	t('你對電力是如何由一開始的發電到最後輸送到我們身邊成為110V/220V的電源過程有興趣嗎?這堂課會教你許多關於電力設備如變壓器等的相關知識!'),
+	t('機率的理論，主要會用在一些通訊模型的建立。而統計的概念，在處理抽樣數據以及檢測結果顯著性的應用上，佔有很重要的角色。'),
 	t('你要修 '+name+' 嗎？'),
 	'CHOICES: ["是","否"]',
 	'CHOICE_0',
@@ -15,13 +15,15 @@ game.ev[map][id] = new game.Ev({
 		'IF: "0 == variable[0]"',
 			s(ev+'.can_take()'),
 			'IF: "1 == variable[0]"',
-				t('請問由三繞組單相變壓器建構在於一單獨線圈上的是哪一種變壓器?'),
-				'CHOICES: ["單相變壓器","三相變壓器","自耦變壓器"]',
+				t('如果一個人在A點投擲一枚硬幣，請問硬幣落在A點的機率是多少？'),
+				'CHOICES: ["0.25","1","0","0.5"]',
 				'CHOICE_0',
 					s(ev+'.fail("答錯了!")'),
 				'CHOICE_1',
-					s(ev+'.take("答對了!!")'),
+					s(ev+'.fail("答錯了!")'),
 				'CHOICE_2',
+					s(ev+'.take("答對了!!")'),
+				'CHOICE_3',
 					s(ev+'.fail("答錯了!")'),
 				'ENDCHOICES',
 			'ENDIF',
