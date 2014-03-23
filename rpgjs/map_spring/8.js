77668238c49e93a3d8517727e3d6aea827bc1f34
Goolley <calvin7899@gmail.com>
2014-03-23 15:16:14 +0800

    add 8.js

---
+++ rpgjs/map_spring/8.js
@@ -0,0 +1,41 @@
+var s = game.Ev.prototype.cmd.script, t = game.Ev.prototype.cmd.text, v0 = game.Ev.prototype.cmd.v0;
+var map = 2, id = 8, ev = 'game.ev['+map+']['+id+']', name = '工程數學(二)';
+game.ev[map][id] = new game.Ev({
+	can_take: function() {
+		return RPGJS.Variables.data[0] = 1;
+		if ( !game.defined( game, 'ev', 1, 10, 'took' ) ) RPGJS.Variables.data[0] = '需要先修 '+game.ev[1][10].name+' ！';
+		else if ( G.player.hp < this.hp_cost() ) RPGJS.Variables.data[0] = '你的體力不夠修這門課囉！';
+		else RPGJS.Variables.data[0] = 1;
+	},
+	hp_cost: function() { return 20; },
+	id: id,
+	map: map,
+	name: name,
+}, [
+	s(ev+'.stop()'),
+	t('經過上學期的介紹，這學期我們將更深入介紹傅立葉級數，在課程的後半段，我們將介紹工程數學在各領域的應用，像是波動方程式，熱方程式等等。'),
+    t('你要修 '+name+' 嗎？'),
+	'CHOICES: ["是","否"]',
+ 	'CHOICE_0',
+		s(ev+'.is_took()'),
+		'IF: "0 == variable[0]"',
+			s(ev+'.can_take()'),
+			'IF: "1 == variable[0]"',
+				t('傅立葉變換是一種線性的積分變換，常將信號在時域和頻域中轉換，在物理學和工程領域種有許多應用。試問關於傅立葉餘弦變換，下列何者錯誤'),
+				'CHOICES: ["f(x)僅定義於大於零的區塊","偶函數","奇函數"]',
+				'CHOICE_0',
+					s(ev+'.fail()'),
+				'CHOICE_1',
+					s(ev+'.fail()'),
+				'CHOICE_2',
+					s(ev+'.take("答對了，恭喜您修過工程數學(二)")'),
+				'ENDCHOICES',
+			'ENDIF',
+		'ENDIF',
+		t('%V[0]'),
+	'CHOICE_1',
+	'ENDCHOICES',
+	s(ev+'.start()'),
+]);
+
+// vi:nowrap:sw=4:ts=4
