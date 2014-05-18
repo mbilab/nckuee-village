499e81e3c8c8a66fc7834f9292af5f649efc08f7
douplus <ccdouplus@gmail.com>
2014-05-18 20:30:14 +0800

    check code and change the logic

--- rpgjs/map_fall/31.js
+++ rpgjs/map_fall/31.js
@@ -1,19 +1,18 @@
 var s = game.Ev.prototype.cmd.script, t = game.Ev.prototype.cmd.text, v0 = game.Ev.prototype.cmd.v0;
 var map = 1, id = 31, ev = 'game.ev['+map+']['+id+']', name = '計算機通訊';
 game.ev[map][id] = new game.Ev({
-		
 	    hp_cost: function() { return 10; },
 	    id: id,
 	    map: map,
 	    name: name,
 }, [
     s(ev+'.stop()'),
+		s(ev+'.is_took()'),
+		'IF: "0 == variable[0]"',
 	t('計算機通訊這門課主要是在介紹無線網路的的各種協定。並透過如程式模擬error rate、網路無線共享、廣播效能模擬、Voip等實驗，讓同學們得以深入瞭解無線網路的相關知識。'),
 	t('你要修 '+name+' 嗎？'),
 	'CHOICES: ["是","否"]',
 	'CHOICE_0',
-		s(ev+'.is_took()'),
-		'IF: "0 == variable[0]"',
 			s(ev+'.can_take()'),
 			'IF: "1 == variable[0]"',
 				t('請問無線網路通用標準的名稱為？'),
@@ -28,10 +27,12 @@ game.ev[map][id] = new game.Ev({
 					s(ev+'.take("答對了！")'),
 				'ENDCHOICES',
 			'ENDIF',
-		'ENDIF',
-		t('%V[0]'),
 	'CHOICE_1',
 	'ENDCHOICES',
+	"ENDIF",
+	'IF: "0 != variable[0]"',
+		t('%V[0]'),
+	"ENDIF",
 	s(ev+'.start()'),
 ]);
 
