305a6f371aa5a871949a58ad29442432b4e47784
douplus <ccdouplus@gmail.com>
2014-05-18 23:52:12 +0800

    change game logic

--- rpgjs/map_spring/22.js
+++ rpgjs/map_spring/22.js
@@ -2,18 +2,17 @@ var s = game.Ev.prototype.cmd.script, t = game.Ev.prototype.cmd.text, v0 = game.
 var map = 2, id = 22, ev = 'game.ev['+map+']['+id+']', name = '微算機與介面設計';
 game.ev[map][id] = new game.Ev({
 	hp_cost: function() { return 20; },
-
 	id: id,
 	map: map,
 	name: name,
 }, [
 	s(ev+'.stop()'),
+		s(ev+'.is_took()'),
+		'IF: "0 == variable[0]"',
 	t('微算機又稱微處理機，是將電腦之中央處理單元建構於單一積體電路晶片。微算機介面是將微算機與周邊裝置連接並運作之硬體與軟體組合。微算機與介面設計探討微處理機之架構與系統介面、基本輸入與輸出介面、介面程式設計等課題。本課程並配合該講授課程內容規劃模組化之實習單元，提供學生深入了解微處理機系統並藉由實機操作與程式設計演練強化學習效果。本課程規劃之內容包含微處理機系統架構與程式設計、基本微算機介面與輸入與輸出程式設計、實習模組硬體與軟體操作說明等與實習模組相關主題之講授。'),
     t('你要修 '+name+' 嗎？'),
 	'CHOICES: ["是","否"]',
  	'CHOICE_0',
-		s(ev+'.is_took()'),
-		'IF: "0 == variable[0]"',
 			s(ev+'.can_take()'),
 			'IF: "1 == variable[0]"',
 				t('ＣＰＵ與記憶體之間的溝通橋樑稱之為匯流排，請問哪一種匯流排不存在？'),
@@ -28,10 +27,12 @@ game.ev[map][id] = new game.Ev({
 					s(ev+'.take("答對了！的確是沒有這東西存在")'),
 				'ENDCHOICES',
 			'ENDIF',
-		'ENDIF',
-		t('%V[0]'),
 	'CHOICE_1',
 	'ENDCHOICES',
+	"ENDIF",
+	'IF: "0 != variable[0]"',
+		t('%V[0]'),
+	"ENDIF",
 	s(ev+'.start()'),
 ]);
 
--- rpgjs/map_spring/25.js
+++ rpgjs/map_spring/25.js
@@ -1,19 +1,18 @@
 var s = game.Ev.prototype.cmd.script, t = game.Ev.prototype.cmd.text, v0 = game.Ev.prototype.cmd.v0;
 var map = 2, id = 25, ev = 'game.ev['+map+']['+id+']', name = '數位通訊';
 game.ev[map][id] = new game.Ev({
-
 	hp_cost: function() { return 12; },
 	id: id,
 	map: map,
 	name: name,
 }, [
 	s(ev+'.stop()'),
+		s(ev+'.is_took()'),
+		'IF: "0 == variable[0]"',
 	t('數位通訊就像是你把紅豆麵包(類比訊號假想物)壓扁,然後切成好幾千塊,並把紅豆和麵包分開,當作1,0。然後你小心地排整齊後，再把它柔在一起，竟然變 成一模一樣的紅豆麵包((不過有一點點沒柔進去(那可能叫做人為的失誤,也可稱為失真)。當然中間的過程怎樣保持紅豆或麵包沒有掉到地上，以及要不要加入 一點糖或是其他東西，就看你如何斟酌阿XD'),
     t('你要修 '+name+' 嗎？'),
 	'CHOICES: ["是","否"]',
  	'CHOICE_0',
-		s(ev+'.is_took()'),
-		'IF: "0 == variable[0]"',
 			s(ev+'.can_take()'),
 			'IF: "1 == variable[0]"',
 				t('QPSK 是什麼?'),
@@ -26,10 +25,12 @@ game.ev[map][id] = new game.Ev({
 					s(ev+'.take("答對了,看來你很喜歡吃紅豆麵包吧")'),
 				'ENDCHOICES',
 			'ENDIF',
-		'ENDIF',
-		t('%V[0]'),
 	'CHOICE_1',
 	'ENDCHOICES',
+	"ENDIF",
+	'IF: "0 != variable[0]"',
+		t('%V[0]'),
+	"ENDIF",
 	s(ev+'.start()'),
 ]);
 
