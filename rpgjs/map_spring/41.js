380a05588e044a8e89b86ecb6184c097568c0fc3
douplus <ccdouplus@gmail.com>
2014-05-19 00:29:42 +0800

    change game logic

--- rpgjs/map_spring/41.js
+++ rpgjs/map_spring/41.js
@@ -1,19 +1,18 @@
 var s = game.Ev.prototype.cmd.script, t = game.Ev.prototype.cmd.text, v0 = game.Ev.prototype.cmd.v0;
 var map = 2, id = 41, ev = 'game.ev['+map+']['+id+']', name = '多媒體網際網路應用';
 game.ev[map][id] = new game.Ev({
-
 	hp_cost: function() { return 18; },
 	id: id,
 	map: map,
 	name: name,
 }, [
 	s(ev+'.stop()'),
+		s(ev+'.is_took()'),
+		'IF: "0 == variable[0]"',
 	t('你知道為什麼網址前面大多都有www嗎？為什麼是三個？不是五個或七個？而http 代表的又是什麼呢？身為電機資訊人，這些還不知道就落伍了，還不趕緊來修課？'),
     t('你要修 '+name+' 嗎？'),
 	'CHOICES: ["是","否"]',
  	'CHOICE_0',
-		s(ev+'.is_took()'),
-		'IF: "0 == variable[0]"',
 			s(ev+'.can_take()'),
 			'IF: "1 == variable[0]"',
 				t('當你在瀏覽網頁時，伺服器無法正確回傳訊息，網頁會出現哪一個數字，代表伺服器發生錯誤？'),
@@ -28,10 +27,12 @@ game.ev[map][id] = new game.Ev({
 					s(ev+'.take("答對了～！！")'),
 				'ENDCHOICES',
 			'ENDIF',
-		'ENDIF',
-		t('%V[0]'),
 	'CHOICE_1',
 	'ENDCHOICES',
+	"ENDIF",
+	'IF: "0 != variable[0]"',
+		t('%V[0]'),
+	"ENDIF",
 	s(ev+'.start()'),
 ]);
 
--- rpgjs/map_spring/42.js
+++ rpgjs/map_spring/42.js
@@ -1,19 +1,18 @@
 var s = game.Ev.prototype.cmd.script, t = game.Ev.prototype.cmd.text, v0 = game.Ev.prototype.cmd.v0;
 var map = 2, id = 42, ev = 'game.ev['+map+']['+id+']', name = '生醫儀器導論';
 game.ev[map][id] = new game.Ev({
-
 	hp_cost: function() { return 15; },
 	id: id,
 	map: map,
 	name: name,
 }, [
 	s(ev+'.stop()'),
+		s(ev+'.is_took()'),
+		'IF: "0 == variable[0]"',
     t('聽過核磁共振嗎？你知道現在癌症除了化療還有一種叫做光子刀的療法嗎？本課程將電機知識結合醫學領域，目地為人類帶來更多的福祉。'),	
     t('你要修 '+name+' 嗎？'),
 	'CHOICES: ["是","否"]',
  	'CHOICE_0',
-		s(ev+'.is_took()'),
-		'IF: "0 == variable[0]"',
 			s(ev+'.can_take()'),
 			'IF: "1 == variable[0]"',
 				t('核磁共振現象來源於原子核的自旋角動量在外加磁場作用下的進動，不同類型的原子核自旋量子數也不同，請問質子數和中子數均為偶數的原子核，其自旋量子數為？'),
@@ -26,10 +25,12 @@ game.ev[map][id] = new game.Ev({
 					s(ev+'.fail("哎呀~答錯了!答案是0唷!")'),
 				'ENDCHOICES',
 			'ENDIF',
-		'ENDIF',
-		t('%V[0]'),
 	'CHOICE_1',
 	'ENDCHOICES',
+	"ENDIF",
+	'IF: "0 != variable[0]"',
+		t('%V[0]'),
+	"ENDIF",
 	s(ev+'.start()'),
 ]);
 
