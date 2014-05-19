6d372f9a75173967a658214fe8eefb6ee0c02045
Goolley <calvin7899@gmail.com>
2014-05-19 20:13:39 +0800

    debug map_spring/10.js

--- rpgjs/map_spring/10.js
+++ rpgjs/map_spring/10.js
@@ -3,7 +3,7 @@ var map = 2, id = 10, ev = 'game.ev['+map+']['+id+']', name = '工程數學(二)
 game.ev[map][id] = new game.Ev({
 	can_take: function() {
 	 	if ( game.player.hp < this.hp_cost() ) return RPGJS.Variables.data[0] = '你的體力不夠修這門課囉！';
-	    if ( !game.defined( game, 'ev', 2, 10, 'is_passed' ) ) return RPGJS.Variables.data[0] ='要通過' + game.ev[2][10].name+ '才可以選修本課！';
+	    if ( !game.defined( game, 'ev', 1, 10, 'is_passed' ) ) return RPGJS.Variables.data[0] ='要通過' + game.ev[1][10].name+ '才可以選修本課！';
 		return RPGJS.Variables.data[0] = 1;
 	},
 
