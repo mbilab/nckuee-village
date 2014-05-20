3cdb2c41b381249bb2c70f11f4708856285c12fd
douplus <ccdouplus@gmail.com>
2014-05-20 17:03:13 +0800

    make map_spring/98.js the same with map_fall

--- rpgjs/map_spring/98.js
+++ rpgjs/map_spring/98.js
@@ -1,13 +1,13 @@
 var s = game.Ev.prototype.cmd.script, t = game.Ev.prototype.cmd.text, v0 = game.Ev.prototype.cmd.v0;
-var map = 2, id = 98, ev = 'game.ev['+map+']['+id+']', name = '教務處鄭先生';
+var map = 1, id = 98, ev = 'game.ev['+map+']['+id+']', name = '教務處鄭先生';
 game.ev[map][id] = new game.Ev({
 	can_graduate: function(){
 		if ( game.player.i_semester < 5 ) return RPGJS.Variables.data[0] = "至少要經過六個學期，才可以畢業喔！";
-		var req = this.check( [2,3,4,5,6,7,8,9,11,14,15], [2,3,4,5,6], 'passed' );
+		var req = this.check( [2,3,4,5,6,7,8,9,11,12,13,14,15,39,40,41,42,43], [2,3,4,5,6,7,8,9,10,11,12,13,35,36,37,38,39,40,41,42], 'is_passed' );
 		if ( req.n_no ) return RPGJS.Variables.data[0] = '尚缺以下 '+req.n_no+' 門必修課喔：\n'+req.no_list;
-		req = this.check( [44,45], [43,44], 'took' );
+		req = this.check( [44,45], [43,44], 'is_took' );
 		if ( req.n_no ) return RPGJS.Variables.data[0] = '尚缺以下 '+req.n_no+' 門必選課喔：\n'+req.no_list;
-		req = this.check( [], [], 'passed' );
+		req = this.check( [], [], 'is_passed' );
 //		if ( req.n_yes < 16 ) return RPGJS.Variables.data[0] = '尚缺 '+(16-req.n_yes)+' 門選修課，還有以下課程可以選擇：\n'+req.no_list;
 
 		RPGJS.System.fadeOutMusic(180);
@@ -19,19 +19,19 @@ game.ev[map][id] = new game.Ev({
 			RPGJS.scene.call('Scene_Gameover');
 		});
 
-		req = this.check( [18,24,28,37], [20,31], 'passed' );
+		req = this.check( [18,24,28,37], [20,31], 'is_passed' );
 		if ( 4 <= req.n_yes ) return game.player.gameover = { bg: 'library', msg: '恭喜順利畢業，因為你有選修 '+req.yes_list+' 等課程，推薦你可以往電通所發展' }
-		req = this.check( [17,21,23,33], [14,23,26,30], 'passed' );
+		req = this.check( [17,21,23,33], [14,23,26,30], 'is_passed' );
 		if ( 4 <= req.n_yes ) return game.player.gameover = { bg: 'library', msg: '恭喜順利畢業，因為你有選修 '+req.yes_list+' 等課程，推薦你可以往電機所材料組發展' }
-		req = this.check( [19,20,26,30], [14,23,26,30], 'passed' );
+		req = this.check( [19,20,26,30], [14,23,26,30], 'is_passed' );
 		if ( 4 <= req.n_yes ) return game.player.gameover = { bg: 'library', msg: '恭喜順利畢業，因為你有選修 '+req.yes_list+' 等課程，推薦你可以往電機所通信組發展' }
-		req = this.check( [23], [], 'passed' );
+		req = this.check( [23], [], 'is_passed' );
 		if ( 1 == req.n_yes ) return game.player.gameover = { bg: 'library', msg: '恭喜順利畢業，因為你有選修 '+req.yes_list+' 等課程，推薦你可以往電機所控制組發展' }
-		req = this.check( [26,28,37], [], 'passed' );
+		req = this.check( [26,28,37], [], 'is_passed' );
 		if ( 3 == req.n_yes ) return game.player.gameover = { bg: 'library', msg: '恭喜順利畢業，因為你有選修 '+req.yes_list+' 等課程，推薦你可以往電機所儀器系統與晶片組發展' }
-		req = this.check( [35,36], [16,19], 'passed' );
+		req = this.check( [35,36], [16,19], 'is_passed' );
 		if ( 4 == req.n_yes ) return game.player.gameover = { bg: 'library', msg: '恭喜順利畢業，因為你有選修 '+req.yes_list+' 等課程，推薦你可以往電機所ＶＬＳＩ／ＣＡＤ組發展' }
-		req = this.check( [30,33,39,43,44], [27,30], 'passed' );
+		req = this.check( [30,33,39,43,44], [27,30], 'is_passed' );
 		if ( 3 <= req.n_yes ) return game.player.gameover = { bg: 'library', msg: '恭喜順利畢業，因為你有選修 '+req.yes_list+' 等課程，推薦你可以往電機所電力組發展' }
 		game.player.gameover = { bg: 'library', msg: '恭喜順利畢業，推薦你可以往微電子所發展' }
 	},
@@ -55,7 +55,7 @@ game.ev[map][id] = new game.Ev({
 		if (ret.n_yes) ret.yes_list = ret.yes_list.substring(0, ret.yes_list.length-1);
 		return ret;
 	},
-	graphic: 2,
+	//graphic: 2,
 	id: id,
 	map: map,
 	name: name,
